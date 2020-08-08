0743c9950103d9887760f95f371fa3f4475239c2
Tony Sorensen <tony48853@gmail.com>
2020-08-08 09:55:40 -0700

    Markup built out in Form.js including three input fields and submit button.

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Form = (params) => {
-  return console.log("This is a form");
+const Form = (props) => {
+
+    const [member, setMember] = useState({
+       name: props.name, email:props.email, role: props.role
+      });
+
+  return (
+  <>
+  <h1>Join The Team!</h1>
+  
+<form>
+<input type="text" name="name" placeholder="Enter Your Name" value={props.name}/>
+<input type="email" name="email" placeholder="Enter Your Email" value={props.email}/>
+<input type="text" name="role" placeholder="Enter Your Role" value={props.role}/>
+<button>Submit</button>
+</form>
+</>
+  )
 };
 
 export default Form;
